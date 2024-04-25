15345fa62b3f1ff3140b52f3c513c7c7c950f318
Raul <raulvara1903@gmail.com>
2024-04-25 15:39:48 +0200

    first commit

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -12,6 +12,6 @@ router.get("/", renderCustomers);
 router.post("/add", createCustomers);
 router.get("/update/:id", editCustomer);
 router.post("/update/:id", updateCustomer);
-router.get("/delete/:id", deleteCustomer);
+router.delete("/delete/:id", deleteCustomer);
 
 export default router;
